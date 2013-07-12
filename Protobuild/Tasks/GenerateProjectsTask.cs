0c04c6e16f8568aa12d75d6beea76053cea9f24a
June Rhodes <jrhodes@redpointgames.com.au>
2013-07-12 23:27:16 +1000

    Fixed null reference and include stack trace when exception is thrown.

--- Protobuild/Tasks/GenerateProjectsTask.cs
+++ Protobuild/Tasks/GenerateProjectsTask.cs
@@ -38,53 +38,61 @@ namespace Protobuild.Tasks
 
         public override bool Execute()
         {
-            this.Log.LogMessage(
-                "Starting generation of projects for " + this.Platform);
-
-            var module = ModuleInfo.Load(Path.Combine(this.RootPath, "Build", "Module.xml"));
-            var definitions = module.GetDefinitionsRecursively().ToArray();
-            
-            // Run Protobuild in batch mode in each of the submodules
-            // where it is present.
-            foreach (var submodule in module.GetSubmodules())
+            try
             {
                 this.Log.LogMessage(
-                    "Invoking submodule generation for " + submodule.Name);
-                submodule.RunProtobuild("-generate " + Platform);
+                    "Starting generation of projects for " + this.Platform);
+    
+                var module = ModuleInfo.Load(Path.Combine(this.RootPath, "Build", "Module.xml"));
+                var definitions = module.GetDefinitionsRecursively().ToArray();
+                
+                // Run Protobuild in batch mode in each of the submodules
+                // where it is present.
+                foreach (var submodule in module.GetSubmodules())
+                {
+                    this.Log.LogMessage(
+                        "Invoking submodule generation for " + submodule.Name);
+                    submodule.RunProtobuild("-generate " + Platform);
+                    this.Log.LogMessage(
+                        "Finished submodule generation for " + submodule.Name);
+                }
+                
+                var generator = new ProjectGenerator(
+                    this.RootPath,
+                    this.Platform,
+                    this.Log);
+                foreach (var definition in definitions)
+                {
+                    this.Log.LogMessage("Loading: " + definition.Name);
+                    generator.Load(Path.Combine(
+                        definition.ModulePath,
+                        "Build",
+                        "Projects",
+                        definition.Name + ".definition"),
+                        module.Path,
+                        definition.ModulePath);
+                }
+                foreach (var definition in definitions.Where(x => x.ModulePath == module.Path))
+                {
+                    this.Log.LogMessage("Generating: " + definition.Name);
+                    generator.Generate(definition.Name);
+                }
+    
+                var solution = Path.Combine(
+                    this.RootPath,
+                    this.ModuleName + "." + this.Platform + ".sln");
+                this.Log.LogMessage("Generating: (solution)");
+                generator.GenerateSolution(solution);
+    
                 this.Log.LogMessage(
-                    "Finished submodule generation for " + submodule.Name);
+                    "Generation complete.");
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex);
+                this.Log.LogError(ex.StackTrace);
             }
             
-            var generator = new ProjectGenerator(
-                this.RootPath,
-                this.Platform,
-                this.Log);
-            foreach (var definition in definitions)
-            {
-                this.Log.LogMessage("Loading: " + definition.Name);
-                generator.Load(Path.Combine(
-                    definition.ModulePath,
-                    "Build",
-                    "Projects",
-                    definition.Name + ".definition"),
-                    module.Path,
-                    definition.ModulePath);
-            }
-            foreach (var definition in definitions.Where(x => x.ModulePath == module.Path))
-            {
-                this.Log.LogMessage("Generating: " + definition.Name);
-                generator.Generate(definition.Name);
-            }
-
-            var solution = Path.Combine(
-                this.RootPath,
-                this.ModuleName + "." + this.Platform + ".sln");
-            this.Log.LogMessage("Generating: (solution)");
-            generator.GenerateSolution(solution);
-
-            this.Log.LogMessage(
-                "Generation complete.");
-
             return true;
         }
     }
