7f5e39bdafad527af13492d7daad568317c7bcd9
June Rhodes <jrhodes@redpointgames.com.au>
2014-03-25 15:57:34 +1100

    Recompress and include changes for MonoMac

---
+++ recompress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Recompressing resources..."
+
+cd Protobuild/bin/Debug
+mono Protobuild.exe --compress ../../BuildResources/GenerateProject.xslt
+mono Protobuild.exe --compress ../../BuildResources/GenerateSolution.xslt
+mono Protobuild.exe --compress ../../../ProtobuildManager/bin/Release/ProtobuildManager.exe
+cp ../../../ProtobuildManager/bin/Release/ProtobuildManager.exe.gz ../../BuildResources/ProtobuildManager.exe.gz
