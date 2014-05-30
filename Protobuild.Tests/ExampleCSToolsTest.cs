4774f2208afbffdb141aa1d3680102256506f11d
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-05-31 00:21:18 +1000

    Added test for cstools-style setup

---
+++ Protobuild.Tests/ExampleCSToolsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class ExampleCSToolsTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("ExampleCSTools");
+
+            this.Generate();
+
+            Assert.True(File.Exists(this.GetPath(@"Game\Game.Windows.csproj")));
+            Assert.True(File.Exists(this.GetPath(@"CSTools\Tool\Tool.Windows.csproj")));
+
+            var gameContents = this.ReadFile(@"Game\Game.Windows.csproj");
+            var solutionContents = this.ReadFile(@"Game.Windows.sln");
+
+            Assert.DoesNotContain("Tool.Windows.csproj", gameContents);
+            Assert.Contains("Game.Windows.csproj", solutionContents);
+            Assert.Contains("Tool.Windows.csproj", solutionContents);
+        }
+    }
+}
