f212f4f071ab53a90e6e3ef59033f90002e9af36
June Rhodes <jrhodes@redpointgames.com.au>
2014-07-03 08:37:04 +1000

    More code style cleanup

--- Protobuild/ModuleInfo.cs
+++ Protobuild/ModuleInfo.cs
@@ -1,102 +1,126 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Xml.Serialization;
-
+//-----------------------------------------------------------------------
+// <copyright file="ModuleInfo.cs" company="Protobuild Project">
+// The MIT License (MIT)
+// 
+// Copyright (c) Various Authors
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+//     The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+// 
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//     THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Protobuild
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// Represents a Protobuild module.
+    /// </summary>
     [Serializable]
     public class ModuleInfo
     {
-        public string Name
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// The root path of this module.
+        /// </summary>
+        [NonSerialized]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+            "Microsoft.StyleCop.CSharp.Maintainability", 
+            "SA1401:FieldsMustBePrivate", 
+            Justification = "This must be a field to allow usage of the NonSerialized attribute.")]
+        public string Path;
 
-        public string DefaultAction { get; set; }
-
-        public string DefaultWindowsPlatforms { get; set; }
-
-        public string DefaultMacOSPlatforms { get; set; }
-
-        public string DefaultLinuxPlatforms { get; set; } 
-
-        public bool GenerateNuGetRepositories { get; set; }
-
-        public string SupportedPlatforms { get; set; } 
-
-        public bool? DisableSynchronisation { get; set; }
-
-        public string[] ModuleAssemblies
-        {
-            get;
-            set;
-        }
-        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Protobuild.ModuleInfo"/> class.
+        /// </summary>
         public ModuleInfo()
         {
             this.ModuleAssemblies = new string[0];
             this.DefaultAction = "resync";
             this.GenerateNuGetRepositories = true;
         }
-        
-        [NonSerialized]
-        public string Path;
-        
-        public BaseTemplate[] GetTemplates()
-        {
-            return (from assembly in this.ModuleAssemblies
-                    let loaded = Assembly.LoadFile(assembly)
-                    from type in loaded.GetTypes()
-                    where !type.IsAbstract
-                    where type.GetConstructor(Type.EmptyTypes) != null
-                    select Activator.CreateInstance(type) as BaseTemplate).ToArray();
-        }
-        
-        public DefinitionInfo[] GetDefinitions()
-        {
-            var result = new List<DefinitionInfo>();
-            var path = System.IO.Path.Combine(Path, "Build", "Projects");
-            foreach (var file in new DirectoryInfo(path).GetFiles("*.definition"))
-            {
-                result.Add(DefinitionInfo.Load(file.FullName));
-            }
-            return result.ToArray();
-        }
-        
-        public IEnumerable<DefinitionInfo> GetDefinitionsRecursively(string relative = "")
-        {
-            foreach (var definition in this.GetDefinitions())
-            {
-                definition.Path = (relative + '\\' + definition.Path).Trim('\\');
-                definition.ModulePath = this.Path;
-                yield return definition;
-            }
-            foreach (var submodule in this.GetSubmodules())
-                foreach (var definition in submodule.GetDefinitionsRecursively((relative + '\\' + submodule.Name).Trim('\\')))
-                    yield return definition;
-        }
-        
-        public ModuleInfo[] GetSubmodules()
-        {
-            var modules = new List<ModuleInfo>();
-            foreach (var directory in new DirectoryInfo(Path).GetDirectories())
-            {
-                var build = directory.GetDirectories().FirstOrDefault(x => x.Name == "Build");
-                if (build == null)
-                    continue;
-                var module = build.GetFiles().FirstOrDefault(x => x.Name == "Module.xml");
-                if (module == null)
-                    continue;
-                modules.Add(ModuleInfo.Load(module.FullName));
-            }
-            return modules.ToArray();
-        }
-        
+
+        /// <summary>
+        /// Gets or sets the name of the module.
+        /// </summary>
+        /// <value>The module name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default action to be taken when Protobuild runs in the module.
+        /// </summary>
+        /// <value>The default action that Protobuild will take.</value>
+        public string DefaultAction { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma seperated list of default platforms when the host platform is Windows.
+        /// </summary>
+        /// <value>The comma seperated list of default platforms when the host platform is Windows.</value>
+        public string DefaultWindowsPlatforms { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma seperated list of default platforms when the host platform is Mac OS.
+        /// </summary>
+        /// <value>The comma seperated list of default platforms when the host platform is Mac OS.</value>
+        public string DefaultMacOSPlatforms { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma seperated list of default platforms when the host platform is Linux.
+        /// </summary>
+        /// <value>The comma seperated list of default platforms when the host platform is Linux.</value>
+        public string DefaultLinuxPlatforms { get; set; } 
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the NuGet repositories.config file is generated.
+        /// </summary>
+        /// <value><c>true</c> if the NuGet repositories.config file is generated; otherwise, <c>false</c>.</value>
+        public bool GenerateNuGetRepositories { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma seperated list of supported platforms in this module.
+        /// </summary>
+        /// <value>The comma seperated list of supported platforms.</value>
+        public string SupportedPlatforms { get; set; } 
+
+        /// <summary>
+        /// Gets or sets a value indicating whether synchronisation is completely disabled in this module.
+        /// </summary>
+        /// <value><c>true</c> if synchronisation is disabled and will be skipped; otherwise, <c>false</c>.</value>
+        public bool? DisableSynchronisation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of .NET assemblies to load when running Protobuild in this module.
+        /// </summary>
+        /// <remarks>
+        /// This list of assemblies is used to load additional templates in the GUI-based module manager.
+        /// </remarks>
+        /// <value>The list of assemblies to load.</value>
+        public string[] ModuleAssemblies { get; set; }
+
+        /// <summary>
+        /// Loads the Protobuild module from the Module.xml file.
+        /// </summary>
+        /// <param name="xmlFile">The path to a Module.xml file.</param>
+        /// <returns>The loaded Protobuild module.</returns>
         public static ModuleInfo Load(string xmlFile)
         {
             var serializer = new XmlSerializer(typeof(ModuleInfo));
@@ -106,7 +130,91 @@ namespace Protobuild
             reader.Close();
             return module;
         }
-        
+
+        /// <summary>
+        /// Gets the list of available project templates for usage in the GUI-based module manager.
+        /// </summary>
+        /// <returns>The available project templates.</returns>
+        public BaseTemplate[] GetTemplates()
+        {
+            return (from assembly in this.ModuleAssemblies
+                    let loaded = Assembly.LoadFile(assembly)
+                    from type in loaded.GetTypes()
+                    where !type.IsAbstract
+                    where type.GetConstructor(Type.EmptyTypes) != null
+                    select Activator.CreateInstance(type) as BaseTemplate).ToArray();
+        }
+
+        /// <summary>
+        /// Loads all of the project definitions present in the current module.
+        /// </summary>
+        /// <returns>The loaded project definitions.</returns>
+        public DefinitionInfo[] GetDefinitions()
+        {
+            var result = new List<DefinitionInfo>();
+            var path = System.IO.Path.Combine(this.Path, "Build", "Projects");
+            foreach (var file in new DirectoryInfo(path).GetFiles("*.definition"))
+            {
+                result.Add(DefinitionInfo.Load(file.FullName));
+            }
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Loads all of the project definitions present in the current module and all submodules.
+        /// </summary>
+        /// <returns>The loaded project definitions.</returns>
+        /// <param name="relative">The current directory being scanned.</param>
+        public IEnumerable<DefinitionInfo> GetDefinitionsRecursively(string relative = "")
+        {
+            foreach (var definition in this.GetDefinitions())
+            {
+                definition.Path = (relative + '\\' + definition.Path).Trim('\\');
+                definition.ModulePath = this.Path;
+                yield return definition;
+            }
+
+            foreach (var submodule in this.GetSubmodules())
+            {
+                foreach (var definition in submodule.GetDefinitionsRecursively((relative + '\\' + submodule.Name).Trim('\\')))
+                {
+                    yield return definition;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads all of the submodules present in this module.
+        /// </summary>
+        /// <returns>The loaded submodules.</returns>
+        public ModuleInfo[] GetSubmodules()
+        {
+            var modules = new List<ModuleInfo>();
+            foreach (var directory in new DirectoryInfo(this.Path).GetDirectories())
+            {
+                var build = directory.GetDirectories().FirstOrDefault(x => x.Name == "Build");
+                if (build == null)
+                {
+                    continue;
+                }
+
+                var module = build.GetFiles().FirstOrDefault(x => x.Name == "Module.xml");
+                if (module == null)
+                {
+                    continue;
+                }
+
+                modules.Add(ModuleInfo.Load(module.FullName));
+            }
+
+            return modules.ToArray();
+        }
+
+        /// <summary>
+        /// Saves the current module to a Module.xml file.
+        /// </summary>
+        /// <param name="xmlFile">The path to a Module.xml file.</param>
         public void Save(string xmlFile)
         {
             var serializer = new XmlSerializer(typeof(ModuleInfo));
@@ -114,7 +222,11 @@ namespace Protobuild
             serializer.Serialize(writer, this);
             writer.Close();
         }
-        
+
+        /// <summary>
+        /// Runs the instance of Protobuild.exe present in the module.
+        /// </summary>
+        /// <param name="args">The arguments to pass to Protobuild.</param>
         public void RunProtobuild(string args)
         {
             var protobuildPath = System.IO.Path.Combine(this.Path, "Protobuild.exe");
@@ -153,6 +265,12 @@ namespace Protobuild
             }
         }
 
+        /// <summary>
+        /// Normalizes the platform string from user input, automatically correcting case
+        /// and validating against a list of supported platforms.
+        /// </summary>
+        /// <returns>The platform string.</returns>
+        /// <param name="platform">The normalized platform string.</param>
         public string NormalizePlatform(string platform)
         {
             var supportedPlatforms = "Android,iOS,Linux,MacOS,Ouya,PSMobile,Windows,Windows8,WindowsGL,WindowsPhone";
@@ -194,4 +312,3 @@ namespace Protobuild
         }
     }
 }
-
--- Protobuild/Options.cs
+++ Protobuild/Options.cs
@@ -1,14 +1,59 @@
-using System;
-using System.Collections.Generic;
-
+//-----------------------------------------------------------------------
+// <copyright file="Options.cs" company="Protobuild Project">
+// The MIT License (MIT)
+// 
+// Copyright (c) Various Authors
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+//     The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+// 
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//     THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Protobuild
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// A simple command line option parser.
+    /// </summary>
     public class Options
     {
-        private Dictionary<string, Action<string[]>> m_Actions = new Dictionary<string, Action<string[]>>();
-        
+        /// <summary>
+        /// The list of registered actions.
+        /// </summary>
+        private Dictionary<string, Action<string[]>> actions = new Dictionary<string, Action<string[]>>();
+
+        /// <summary>
+        /// Gets or sets the action to take for various command line arguments.
+        /// </summary>
+        /// <param name="key">The command line option.</param>
+        /// <returns>The action to take.</returns>
+        public Action<string[]> this[string key]
+        {
+            get { return this.actions[key.ToLower()]; }
+            set { this.actions[key.ToLower()] = value; }
+        }
+
+        /// <summary>
+        /// Parse the command line arguments.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
         public void Parse(string[] args)
         {
             for (var i = 0; i < args.Length; i++)
@@ -18,7 +63,7 @@ namespace Protobuild
                     arg.StartsWith("/", StringComparison.InvariantCulture))
                 {
                     var realArg = arg.TrimStart('-').TrimStart('/').ToLower();
-                    if (!this.m_Actions.Keys.Any(x => x.StartsWith(realArg + "@") || string.Compare(x, realArg, StringComparison.OrdinalIgnoreCase) == 0))
+                    if (!this.actions.Keys.Any(x => x.StartsWith(realArg + "@") || string.Compare(x, realArg, StringComparison.OrdinalIgnoreCase) == 0))
                     {
                         throw new InvalidOperationException("Unknown argument '" + arg + "'");
                     }
@@ -34,34 +79,48 @@ namespace Protobuild
                             // We can't break on the / character, as this is used in paths
                             // on Linux and Mac OS.
                             if (args[i].StartsWith("-", StringComparison.InvariantCulture))
+                            {
                                 break;
+                            }
 
                             actionArgs.Add(args[i]);
                         }
                     }
+
                     while (actionArgs.Count < takeArgs)
+                    {
                         actionArgs.Add(null);
-                    if (this.m_Actions.ContainsKey(realArg))
-                        this.m_Actions[realArg](actionArgs.ToArray());
-                    if (this.m_Actions.ContainsKey(realArg + "@" + takeArgs))
-                        this.m_Actions[realArg + "@" + takeArgs](actionArgs.ToArray());
+                    }
+
+                    if (this.actions.ContainsKey(realArg))
+                    {
+                        this.actions[realArg](actionArgs.ToArray());
+                    }
+
+                    if (this.actions.ContainsKey(realArg + "@" + takeArgs))
+                    {
+                        this.actions[realArg + "@" + takeArgs](actionArgs.ToArray());
+                    }
                 }
             }
         }
-        
+
+        /// <summary>
+        /// Gets the parameter count for the specified argument key.
+        /// </summary>
+        /// <returns>The parameter count for the specified argument key.</returns>
+        /// <param name="arg">The argument key.</param>
         private int GetParameterCountForArgument(string arg)
         {
-            foreach (string key in this.m_Actions.Keys)
+            foreach (string key in this.actions.Keys)
+            {
                 if (key.StartsWith(arg + "@", StringComparison.Ordinal))
-                    return Convert.ToInt32(key.Split(new[]{'@'}, 2)[1]);
+                {
+                    return Convert.ToInt32(key.Split(new[] { '@' }, 2)[1]);
+                }
+            }
+
             return 0;
-        }
-        
-        public Action<string[]> this[string key]
-        {
-            get { return this.m_Actions[key.ToLower()]; }
-            set { this.m_Actions[key.ToLower()] = value; }
         }
     }
 }
-
