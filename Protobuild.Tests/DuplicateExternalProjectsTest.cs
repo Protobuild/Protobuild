aaa4b700e2f1ff5b449f9f0c6ef61bc0d886cf1d
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-08-24 11:15:37 +1000

    Prevent duplicate external projects in solution
    Fixes #52.

---
+++ Protobuild.Tests/DuplicateExternalProjectsTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class DuplicateExternalProjectsTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("DuplicateExternalProjects");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"DuplicateExternalProjects.Windows.sln")));
+
+            var solutionContents = this.ReadFile(@"DuplicateExternalProjects.Windows.sln");
+
+            Assert.Contains("ThirdParty1.csproj", solutionContents);
+            Assert.DoesNotContain("ThirdParty2.csproj", solutionContents);
+        }
+    }
+}
