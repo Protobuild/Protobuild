c3cc0b7029ca1d7c0a852fce4a000b0211253dbe
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-08-24 11:40:55 +1000

    Enable "PlatformSpecificOutputFolder" by default
    Fixes #56.

---
+++ Protobuild.Tests/PlatformSpecificOutputDefaultTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class PlatformSpecificOutputDefaultTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("PlatformSpecificOutputDefault");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Test\Test.Windows.csproj")));
+
+            var projectContents = this.ReadFile(@"Test\Test.Windows.csproj");
+
+            Assert.Contains(@"bin\Windows\AnyCPU\Release", projectContents);
+            Assert.DoesNotContain(@"bin\Release", projectContents);
+        }
+    }
+}
---
+++ Protobuild.Tests/PlatformSpecificOutputFalseTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class PlatformSpecificOutputFalseTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("PlatformSpecificOutputFalse");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Test\Test.Windows.csproj")));
+
+            var projectContents = this.ReadFile(@"Test\Test.Windows.csproj");
+
+            Assert.DoesNotContain(@"bin\Windows\AnyCPU\Release", projectContents);
+            Assert.Contains(@"bin\Release", projectContents);
+        }
+    }
+}
