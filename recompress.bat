f1abce4e75c8e26ca6db4980b3eace4075746412
Alejandro Jimenez Martinez <aljim@microsoft.com>
2014-09-16 17:01:17 -0700

    recompress and bootstrap Protobuild with Windows Phone 8.1 definitions

--- recompress.bat
+++ recompress.bat
@@ -1,9 +1,9 @@
 echo "Recompressing resources..."
 
 cd %~dp0
-cd Protobuild\bin\Debug
-Protobuild.exe --compress ..\..\BuildResources\GenerateProject.xslt
-Protobuild.exe --compress ..\..\BuildResources\GenerateSolution.xslt
-Protobuild.exe --compress ..\..\BuildResources\JSILTemplate.htm
-Protobuild.exe --compress ..\..\..\ProtobuildManager\bin\Release\ProtobuildManager.exe
+cd Protobuild\bin\Windows\AnyCPU\Release
+Protobuild.exe --compress ..\..\..\..\BuildResources\GenerateProject.xslt
+Protobuild.exe --compress ..\..\..\..\BuildResources\GenerateSolution.xslt
+Protobuild.exe --compress ..\..\..\..\BuildResources\JSILTemplate.htm
+Protobuild.exe --compress ..\..\..\..\..\ProtobuildManager\bin\Release\ProtobuildManager.exe
 xcopy /Y ..\..\..\ProtobuildManager\bin\Release\ProtobuildManager.exe.gz ..\..\BuildResources\ProtobuildManager.exe.gz
