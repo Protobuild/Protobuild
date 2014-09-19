17441e5e64d8fa6d5e3e8323f1a8ae60676dc0a0
June Rhodes <jrhodes@redpointgames.com.au>
2014-09-19 12:45:54 +1000

    Recompress and rebuild

--- recompress.sh
+++ recompress.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Recompressing resources..."
 
-cd Protobuild/bin/Debug
-mono Protobuild.exe --compress ../../BuildResources/GenerateProject.xslt
-mono Protobuild.exe --compress ../../BuildResources/GenerateSolution.xslt
-mono Protobuild.exe --compress ../../BuildResources/JSILTemplate.htm
-mono Protobuild.exe --compress ../../../ProtobuildManager/bin/Release/ProtobuildManager.exe
-cp ../../../ProtobuildManager/bin/Release/ProtobuildManager.exe.gz ../../BuildResources/ProtobuildManager.exe.gz
+cd Protobuild/bin/Linux/AnyCPU/Debug
+mono Protobuild.exe --compress ../../../../BuildResources/GenerateProject.xslt
+mono Protobuild.exe --compress ../../../../BuildResources/GenerateSolution.xslt
+mono Protobuild.exe --compress ../../../../BuildResources/JSILTemplate.htm
+mono Protobuild.exe --compress ../../../../../ProtobuildManager/bin/Linux/AnyCPU/Release/ProtobuildManager.exe
+cp ../../../../../ProtobuildManager/bin/Linux/AnyCPU/Release/ProtobuildManager.exe.gz ../../../../BuildResources/ProtobuildManager.exe.gz
