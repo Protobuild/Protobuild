8da01e798e5dd11452698c74e1c499651d20a729
June Rhodes <jrhodes@redpointgames.com.au>
2014-10-12 16:43:14 +1100

    Add ProjectSpecificOutputFolder option

---
+++ Protobuild.Tests/ProjectSpecificOutputFolderTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class ProjectSpecificOutputFolderTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("ProjectSpecificOutputFolder");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"ConsoleA\ConsoleA.Windows.csproj")));
+            Assert.True(File.Exists(this.GetPath(@"ConsoleB\ConsoleB.Windows.csproj")));
+
+            var consoleAContents = this.ReadFile(@"ConsoleA\ConsoleA.Windows.csproj");
+            var consoleBContents = this.ReadFile(@"ConsoleB\ConsoleB.Windows.csproj");
+
+            Assert.Contains("<OutputPath>bin\\Windows\\AnyCPU\\Debug</OutputPath>", consoleAContents);
+            Assert.DoesNotContain("<OutputPath>bin\\ConsoleA\\Windows\\AnyCPU\\Debug</OutputPath>", consoleAContents);
+            Assert.Contains("<OutputPath>bin\\ConsoleB\\Windows\\AnyCPU\\Debug</OutputPath>", consoleBContents);
+            Assert.DoesNotContain("<OutputPath>bin\\Windows\\AnyCPU\\Debug</OutputPath>", consoleBContents);
+        }
+    }
+}
