70243dd70859f1946d3f0f0b061672d1012422ab
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-10-16 12:12:29 +0200

    Changes to ContentProject for Windows and debug symbols in release mode
    Debug symbols in release mode are now controlled by the DebugSymbolsOnRelease property.  Previously debug symbols in release were enabled by default; they now default to off and can be re-enabled with `<Property Name="DebugSymbolsOnRelease" Value="full" />`.
    
    Projects generated for Windows now flag their content project as `<Content>` instead of `<None>`, which has changed to assist usage with installers that automatically detect file build types.

--- recompress.sh
+++ recompress.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+set -x
+
+if [[ `uname` == Darwin ]]; then
+  PLATFORM=MacOS
+else
+  PLATFORM=Linux
+fi
+
 echo "Recompressing resources..."
 
-cd Protobuild/bin/Linux/AnyCPU/Debug
+cd Protobuild/bin/$PLATFORM/AnyCPU/Debug
 mono Protobuild.exe --compress ../../../../BuildResources/GenerateProject.xslt
 mono Protobuild.exe --compress ../../../../BuildResources/GenerateSolution.xslt
 mono Protobuild.exe --compress ../../../../BuildResources/SelectSolution.xslt
 mono Protobuild.exe --compress ../../../../BuildResources/JSILTemplate.htm
-mono Protobuild.exe --compress ../../../../../ProtobuildManager/bin/Linux/AnyCPU/Release/ProtobuildManager.exe
-cp ../../../../../ProtobuildManager/bin/Linux/AnyCPU/Release/ProtobuildManager.exe.gz ../../../../BuildResources/ProtobuildManager.exe.gz
+mono Protobuild.exe --compress ../../../../../ProtobuildManager/bin/$PLATFORM/AnyCPU/Release/ProtobuildManager.exe
+cp ../../../../../ProtobuildManager/bin/$PLATFORM/AnyCPU/Release/ProtobuildManager.exe.gz ../../../../BuildResources/ProtobuildManager.exe.gz
