6caabe5da9ad662fd8f6380da7d12217e6831d48
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-10-16 18:39:10 +0200

    Changed build scripts to work on MacOS

--- rebuild.sh
+++ rebuild.sh
@@ -3,9 +3,15 @@
 set -e
 set -x
 
-mono Protobuild.exe --generate
-xbuild /p:Configuration=Debug Protobuild.Linux.sln
+if [[ `uname` == Darwin ]]; then
+  PLATFORM=MacOS
+else
+  PLATFORM=Linux
+fi
+
+mono Protobuild.exe --generate $PLATFORM
+xbuild /p:Configuration=Debug Protobuild.$PLATFORM.sln
 ./recompress.sh
-xbuild /p:Configuration=Release Protobuild.Linux.sln
-cp Protobuild/bin/Linux/AnyCPU/Release/Protobuild.exe ./
-mono Protobuild.exe --generate
+xbuild /p:Configuration=Release Protobuild.$PLATFORM.sln
+cp Protobuild/bin/$PLATFORM/AnyCPU/Release/Protobuild.exe ./
+mono Protobuild.exe --generate $PLATFORM
