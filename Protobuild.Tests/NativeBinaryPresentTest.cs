fb8be573fd28796e5a0c3dc770ff3e3b4cb2d41a
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-10-25 17:25:40 +1100

    Add support for <NativeBinary>
    The <NativeBinary> tag in external projects allows you to reference native libraries, ensuring that they are copied correctly to your application or library output directory on build.
    
    See https://github.com/hach-que/Protobuild/wiki/References-and-Projects#nativebinary for more information.

---
+++ Protobuild.Tests/NativeBinaryPresentTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class NativeBinaryPresentTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("NativeBinaryPresent");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Module.Windows.sln")));
+            Assert.True(File.Exists(this.GetPath(@"Console\Console.Windows.csproj")));
+
+            var projectContents = this.ReadFile(@"Console\Console.Windows.csproj");
+
+            Assert.Contains("<Link>All</Link>", projectContents);
+            Assert.Contains("<Link>Windows.dll</Link>", projectContents);
+            Assert.DoesNotContain("<Link>Linux.so</Link>", projectContents);
+            Assert.Contains("<CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>", projectContents);
+            Assert.Contains(@"<None Include=""..\ThirdParty\All"">", projectContents);
+            Assert.Contains(@"<None Include=""..\ThirdParty\Windows.dll"">", projectContents);
+            Assert.DoesNotContain(@"<None Include=""..\ThirdParty\Linux.so"">", projectContents);
+
+            this.Generate("Linux");
+
+            Assert.True(File.Exists(this.GetPath(@"Module.Linux.sln")));
+            Assert.True(File.Exists(this.GetPath(@"Console\Console.Linux.csproj")));
+
+            projectContents = this.ReadFile(@"Console\Console.Linux.csproj");
+
+            Assert.Contains("<Link>All</Link>", projectContents);
+            Assert.DoesNotContain("<Link>Windows.dll</Link>", projectContents);
+            Assert.Contains("<Link>Linux.so</Link>", projectContents);
+            Assert.Contains("<CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>", projectContents);
+            Assert.Contains(@"<None Include=""..\ThirdParty\All"">", projectContents);
+            Assert.DoesNotContain(@"<None Include=""..\ThirdParty\Windows.dll"">", projectContents);
+            Assert.Contains(@"<None Include=""..\ThirdParty\Linux.so"">", projectContents);
+        }
+    }
+}
