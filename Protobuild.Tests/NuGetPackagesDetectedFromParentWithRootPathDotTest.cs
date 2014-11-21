14bc6d8cb1d5c0aa750401e8376689ff2f67f56b
June Rhodes <jrhodes@redpointgames.com.au>
2014-11-21 21:41:49 +1100

    Add tests for parent NuGet packages with root projects

---
+++ Protobuild.Tests/NuGetPackagesDetectedFromParentWithRootPathDotTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class NuGetPackagesDetectedFromParentWithRootPathDotTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("NuGetPackagesDetectedFromParentWithRootPathDot");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Module.Windows.sln")));
+            Assert.True(File.Exists(this.GetPath(@"Submodule\Module.Windows.sln")));
+            Assert.True(File.Exists(this.GetPath(@"Submodule\Console.Windows.csproj")));
+
+            var consoleContents = this.ReadFile(@"Submodule\Console.Windows.csproj");
+
+            Assert.Contains("portable-net4+sl5+wp8+win8+wpa81+MonoTouch+MonoAndroid", consoleContents);
+            Assert.Contains("Test.dll", consoleContents);
+            Assert.Contains("<HintPath>..\\packages", consoleContents);
+            Assert.DoesNotContain("<HintPath>packages", consoleContents);
+            Assert.DoesNotContain("<HintPath>..\\..\\packages", consoleContents);
+        }
+    }
+}
