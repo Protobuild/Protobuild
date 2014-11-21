ee02a0daf92f1b17b8a16edbdaf3a03bc638f746
June Rhodes <jrhodes@redpointgames.com.au>
2014-11-21 17:58:09 +1100

    Update the generation to support the new iOS API

---
+++ Protobuild.Tests/iOSLegacyAPIGenerationWorksTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class iOSLegacyAPIGenerationWorksTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("iOSLegacyAPIGenerationWorks");
+
+            this.Generate("iOS");
+
+            Assert.True(File.Exists(this.GetPath(@"App\App.iOS.csproj")));
+
+            var appContents = this.ReadFile(@"App\App.iOS.csproj");
+
+            Assert.DoesNotContain(@"Xamarin.iOS", appContents);
+            Assert.DoesNotContain(@"$(MSBuildExtensionsPath)\Xamarin\iOS\Xamarin.iOS.CSharp.targets", appContents);
+            Assert.Contains(@"monotouch", appContents);
+            Assert.Contains(@"$(MSBuildToolsPath)\Microsoft.CSharp.targets", appContents);
+        }
+    }
+}
---
+++ Protobuild.Tests/iOSUnifiedAPIGenerationWorksTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class iOSUnifiedAPIGenerationWorksTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("iOSUnifiedAPIGenerationWorks");
+
+            this.Generate("iOS");
+
+            Assert.True(File.Exists(this.GetPath(@"App\App.iOS.csproj")));
+
+            var appContents = this.ReadFile(@"App\App.iOS.csproj");
+
+            Assert.Contains(@"Xamarin.iOS", appContents);
+            Assert.Contains(@"$(MSBuildExtensionsPath)\Xamarin\iOS\Xamarin.iOS.CSharp.targets", appContents);
+            Assert.DoesNotContain(@"monotouch", appContents);
+            Assert.DoesNotContain(@"$(MSBuildToolsPath)\Microsoft.CSharp.targets", appContents);
+        }
+    }
+}
