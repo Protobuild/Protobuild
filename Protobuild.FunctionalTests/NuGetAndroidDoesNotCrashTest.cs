8d558c0b1007277a7c078ec23e232bd5b57b1d3c
James Rhodes <jrhodes@redpointsoftware.com.au>
2015-02-13 10:15:50 +1100

    Fix crash when detecting Android NuGet packages

---
+++ Protobuild.FunctionalTests/NuGetAndroidDoesNotCrashTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class NuGetAndroidDoesNotCrashTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("NuGetAndroidDoesNotCrash");
+
+            this.Generate("Android");
+
+            Assert.True(File.Exists(this.GetPath(@"Module.Android.sln")));
+            Assert.True(File.Exists(this.GetPath(@"Console\Console.Android.csproj")));
+
+            var consoleContents = this.ReadFile(@"Console\Console.Android.csproj");
+
+            Assert.Contains("MonoAndroid10", consoleContents);
+            Assert.Contains("MonoAndroid403", consoleContents);
+            Assert.Contains("MonoAndroid41", consoleContents);
+        }
+    }
+}
