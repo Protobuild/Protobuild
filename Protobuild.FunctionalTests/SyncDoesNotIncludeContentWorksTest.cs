e4ae4d1f00a5be60fadfeee5b1cc582c880ad0f6
James Rhodes <jrhodes@redpointsoftware.com.au>
2015-03-02 13:24:09 +1100

    Add test for basic synchronisation with content projects

---
+++ Protobuild.FunctionalTests/SyncDoesNotIncludeContentWorksTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class SyncDoesNotIncludeContentWorksTest : ProtobuildTest
+    {
+        [Fact]
+        public void SyncIsCorrect()
+        {
+            this.SetupTest("SyncDoesNotIncludeContentWorks");
+
+            // Reset the definition back to it's original version for the test.
+            var original = this.GetPath(@"Build\OriginalProjects\Console.definition");
+            var target = this.GetPath(@"Build\Projects\Console.definition");
+            File.Copy(original, target, true);
+
+            this.OtherMode("sync", "Windows", purge: false);
+
+            var targetContents = this.ReadFile(@"Build\Projects\Console.definition");
+
+            Assert.Contains("Program.cs", targetContents);
+            Assert.Contains("Added.cs", targetContents);
+            Assert.DoesNotContain("Content", targetContents);
+
+            // Reset the file back after the test passes so that Git doesn't
+            // report this file as changed.
+            File.Copy(original, target, true);
+        }
+    }
+}
