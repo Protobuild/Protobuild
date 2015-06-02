ea048816c0cfcd8b3ce08e42fe1029547992ff0f
J Rhodes <jrhodes@redpointsoftware.com.au>
2015-06-02 19:57:51 +1000

    Add support for WarningLevel and TreatWarningsAsErrors properties

---
+++ Protobuild.FunctionalTests/TreatWarningsAsErrorsPropertyTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class TreatWarningsAsErrorsPropertyTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("TreatWarningsAsErrorsProperty");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Test\Test.Windows.csproj")));
+
+            var projectContents = this.ReadFile(@"Test\Test.Windows.csproj");
+
+            Assert.Contains(@"<TreatWarningsAsErrors>TREAT_WARNINGS_AS_ERRORS_TEST</TreatWarningsAsErrors>", projectContents);
+        }
+    }
+}
---
+++ Protobuild.FunctionalTests/WarningLevelPropertyTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class WarningLevelPropertyTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("WarningLevelProperty");
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(this.GetPath(@"Test\Test.Windows.csproj")));
+
+            var projectContents = this.ReadFile(@"Test\Test.Windows.csproj");
+
+            Assert.Contains(@"<WarningLevel>WARNING_LEVEL_TEST</WarningLevel>", projectContents);
+        }
+    }
+}
