4efe82f0cf5199b04cc4a692785df62f784a06ca
Katelyn Gigante <clockwork.singularity@gmail.com>
2015-09-16 23:02:28 +1000

    Add ability to change the Protobuild cache directory by creating an protobuild-cache-directory.txt file
    Create a file in %appdata%\protobuild-cache-directory.txt with the full path to the cache directory you want to use (e.g. if you want to move the cache onto a different hard drive), and Protobuild will automatically migrate and use the new cache location.

--- Protobuild.Internal/Packages/PackageCache/PackageCacheConfiguration.cs
+++ Protobuild.Internal/Packages/PackageCache/PackageCacheConfiguration.cs
@@ -8,8 +8,71 @@ namespace Protobuild
         public string GetCacheDirectory()
         {
             var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var directory = Path.Combine(appData, ".protobuild-cache");
-            Directory.CreateDirectory(directory);
+            var cacheDirectory = Path.Combine(appData, "protobuild-cache-directory.txt");
+            var defaultCacheDirectory = Path.Combine(appData, ".protobuild-cache");
+            string directory;
+
+            if (File.Exists(cacheDirectory))
+            {
+                using (var reader = new StreamReader(cacheDirectory))
+                {
+                    directory = reader.ReadToEnd().Trim();
+                }
+            }
+            else
+            {
+                directory = defaultCacheDirectory;
+            }
+
+            try
+            {
+                Directory.CreateDirectory(directory);
+            }
+            catch
+            {
+                Console.WriteLine(
+                    "WARNING: Cache directory '" + directory + "' specified in " + 
+                    cacheDirectory + 
+                    " could not be created.  Using default cache directory.");
+                directory = defaultCacheDirectory;
+            }
+
+            // Inline check: If the cache directory is not the default cache directory, move
+            // all files and folders from the default cache directory (if it exists) to the
+            // cache directory.  This allows users to migrate existing cache data automatically.
+            if (directory != defaultCacheDirectory && Directory.Exists(defaultCacheDirectory))
+            {
+                Console.WriteLine("New cache directory detected, migrating existing data...");
+                var directoryInfo = new DirectoryInfo(defaultCacheDirectory);
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    Console.WriteLine("Moving cache file " + file.Name + "...");
+                    if (File.Exists(Path.Combine(directory, file.Name)))
+                    {
+                        File.Delete(Path.Combine(directory, file.Name));
+                    }
+                    file.MoveTo(Path.Combine(directory, file.Name));
+                }
+                foreach (var subdirectory in directoryInfo.GetDirectories())
+                {
+                    Console.WriteLine("Moving cache directory " + subdirectory.Name + "...");
+                    if (Directory.Exists(Path.Combine(directory, subdirectory.Name)))
+                    {
+                        Directory.Delete(Path.Combine(directory, subdirectory.Name), true);
+                    }
+                    subdirectory.MoveTo(Path.Combine(directory, subdirectory.Name));
+                }
+
+                try
+                {
+                    Directory.Delete(defaultCacheDirectory);
+                }
+                catch
+                {
+                    Console.WriteLine("WARNING: Unable to remove default cache directory!");
+                }
+            }
+
             return directory;
         }
 
