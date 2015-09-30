a31b25dbe1630db9b418726f3069e6622d93c603
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-10-01 02:00:56 +1000

    Use custom serialization for Module.xml
    Closes #121

---
+++ Protobuild.FunctionalTests/ModuleInfoGetsUpgradedTest.cs
@@ -0,0 +1,36 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Xunit;
+
+    public class ModuleInfoGetsUpgradedTest : ProtobuildTest
+    {
+        [Fact]
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("ModuleInfoGetsUpgraded");
+
+            if (File.Exists(GetPath(Path.Combine("Build", "Module.xml"))))
+            {
+                File.Delete(GetPath(Path.Combine("Build", "Module.xml")));
+            }
+
+            File.Copy(
+                GetPath(Path.Combine("Build", "ModuleUnconverted.xml")),
+                GetPath(Path.Combine("Build", "Module.xml")));
+
+            this.Generate("Windows");
+
+            Assert.True(File.Exists(GetPath(Path.Combine("Build", "Module.xml"))));
+            Assert.Equal(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Module>
+  <Name>Protobuild</Name>
+  <DefaultAction>resync</DefaultAction>
+  <GenerateNuGetRepositories>true</GenerateNuGetRepositories>
+  <Packages>
+    <Package Uri=""http://protobuild.org/hach-que/xunit"" Folder=""xunit"" GitRef=""master"" />
+  </Packages>
+</Module>", ReadFile(Path.Combine("Build", "Module.xml")).Trim());
+        }
+    }
+}
