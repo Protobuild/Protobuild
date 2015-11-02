76296368a705b04f86c66f74efd581a670e07531
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-11-02 14:39:58 +1100

    Add --execute-configuration to set the preferred configuration for --execute

---
+++ Protobuild.Internal/CommandLine/ExecuteConfigurationCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Protobuild
+{
+    public class ExecuteConfigurationCommand : ICommand
+    {
+        public void Encounter(Execution pendingExecution, string[] args)
+        {
+            pendingExecution.ExecuteProjectConfiguration = args[0];
+        }
+
+        public int Execute(Execution execution)
+        {
+            throw new NotSupportedException();
+        }
+
+        public string GetDescription()
+        {
+            return @"
+Sets the preferred configuration when executing binaries in C#
+projects.  When you build in both Debug and Release modes, there
+will be multiple binaries for a given C# project.  By default,
+Protobuild will prefer the Debug version, but you can use this
+option to explicitly set which configuration should be used.
+";
+        }
+
+        public int GetArgCount()
+        {
+            return 1;
+        }
+
+        public string[] GetArgNames()
+        {
+            return new[] { "configuration" };
+        }
+    }
+}
+
