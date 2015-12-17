65606380e6b65fce46777cab4dfefd2329c607b4
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-12-17 17:41:13 +1100

    Do not initialize a package if the target folder isn't managed by Protobuild
    We now prevent Protobuild from initializing packages into their target
    folders if the target folder contains at least 1 file or subdirectory,
    but doesn't a .pkg file or a .git file or subdirectory.  When these
    files are not present, it indicates that something else initialized data
    into the target directory prior to Protobuild running and hence there's
    no safe way of removing that data to put the package in-place.
    
    This scenario occurs on some build servers like TeamCity, which clone
    Git submodules but don't place the .git file that is normally present
    for a submodule folder.  Previously Protobuild would delete the copy
    that TeamCity placed there and re-clone from the cache.

---
+++ Protobuild.FunctionalTests/PackageResolutionDoesNotOccurInFolderWithExistingDataTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Prototest.Library.Version1;
+
+    public class PackageResolutionDoesNotOccurInFolderWithExistingDataTest : ProtobuildTest
+    {
+        private readonly IAssert _assert;
+
+        public PackageResolutionDoesNotOccurInFolderWithExistingDataTest(IAssert assert) : base(assert)
+        {
+            _assert = assert;
+        }
+
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("PackageResolutionDoesNotOccurInFolderWithExistingData");
+
+            var src = this.SetupSrcPackage();
+
+            this.Generate(args: "--redirect http://protobuild.org/hach-que/TestEmptyPackage local-git://" + src);
+
+            _assert.True(File.Exists(this.GetPath("Package\\empty.txt")));
+            _assert.False(File.Exists(this.GetPath("Package\\.pkg")));
+            _assert.False(File.Exists(this.GetPath("Package\\.git")));
+            _assert.False(Directory.Exists(this.GetPath("Package\\.git")));
+        }
+    }
+}
