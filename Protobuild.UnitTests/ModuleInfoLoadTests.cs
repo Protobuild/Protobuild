480d05cd18d0a63c6a0f84fcbe0f22e251414a86
June Rhodes <jrhodes@redpointsoftware.com.au>
2016-11-20 00:15:13 +1100

    Support new package declaration format in Module.xml (#180)
    As outlined in #177, this supports the new <Package> attributes which are used for packages store in a NuGet v3 API repository.

--- Protobuild.UnitTests/ModuleInfoLoadTests.cs
+++ Protobuild.UnitTests/ModuleInfoLoadTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Prototest.Library.Version1;
 
 namespace Protobuild.UnitTests
@@ -44,6 +45,57 @@ namespace Protobuild.UnitTests
             _assert.Equal(2, module.FeatureSet.Count);
             _assert.Contains(Feature.PackageManagement, module.FeatureSet);
             _assert.Contains(Feature.HostPlatformGeneration, module.FeatureSet);
+        }
+
+        public void TestPackagesAreReadCorrectly()
+        {
+            var module = LoadModuleXml("Package.xml");
+
+            _assert.NotNull(module.Packages);
+            _assert.Equal(2, module.Packages.Count);
+
+            _assert.Equal("https://protobuild.org/hach-que/Protobuild", module.Packages[0].Uri);
+            _assert.Equal("master", module.Packages[0].GitRef);
+            _assert.Equal("Protobuild", module.Packages[0].Folder);
+
+            _assert.Equal("https-nuget-v3://api.nuget.org/v3/index.json|Protobuild", module.Packages[1].Uri);
+            _assert.Equal("16.1119.13506", module.Packages[1].GitRef);
+            _assert.Equal("Protobuild", module.Packages[1].Folder);
+        }
+
+        public void TestPackagesAreWrittenCorrectly()
+        {
+            var module = LoadModuleXml("Package.xml");
+
+            _assert.NotNull(module.Packages);
+            _assert.Equal(2, module.Packages.Count);
+
+            _assert.Equal("https://protobuild.org/hach-que/Protobuild", module.Packages[0].Uri);
+            _assert.Equal("master", module.Packages[0].GitRef);
+            _assert.Equal("Protobuild", module.Packages[0].Folder);
+            
+            _assert.Equal("https-nuget-v3://api.nuget.org/v3/index.json|Protobuild", module.Packages[1].Uri);
+            _assert.Equal("16.1119.13506", module.Packages[1].GitRef);
+            _assert.Equal("Protobuild", module.Packages[1].Folder);
+
+            using (var memory = new MemoryStream())
+            {
+                module.Save(memory);
+                memory.Seek(0, SeekOrigin.Begin);
+
+                var stream = typeof(ModuleInfoLoadTests).Assembly.GetManifestResourceStream("Package.xml");
+
+                using (var reader1 = new StreamReader(memory))
+                {
+                    using (var reader2 = new StreamReader(stream))
+                    {
+                        var text1 = reader1.ReadToEnd();
+                        var text2 = reader2.ReadToEnd();
+
+                        _assert.Equal(text1, text2);
+                    }
+                }
+            }
         }
 
         private ModuleInfo LoadModuleXml(string manifestStreamName)
