fd6a948fc7e1ae527a413ae5ac3af84054f7e691
June Rhodes <jrhodes@redpointsoftware.com.au>
2017-02-24 00:25:43 +1100

    Handle NuGet packages that have packageContent under a different node (#228)

---
+++ Protobuild.FunctionalTests/NuGet3PackageContentFallbackTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Protobuild.Tests
+{
+    using System.IO;
+    using Prototest.Library.Version1;
+
+    public class NuGet3PackageContentFallbackTest : ProtobuildTest
+    {
+        private readonly IAssert _assert;
+
+        public NuGet3PackageContentFallbackTest(IAssert assert) : base(assert)
+        {
+            _assert = assert;
+        }
+
+        public void GenerationIsCorrect()
+        {
+            this.SetupTest("NuGet3PackageContentFallback");
+
+            // This asserts the command succeeds.
+            this.OtherMode("resolve", "Windows");
+        }
+    }
+}
