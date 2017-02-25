965d17f298aa61c777261eb55cf6714b76201cda
June Rhodes <jrhodes@redpointgames.com.au>
2017-02-26 01:45:36 +1100

    Remove unnecessary Info.plist in test projects

---
+++ Protobuild.FunctionalTests/Program.cs
@@ -0,0 +1,35 @@
+﻿#if PLATFORM_IOS
+using UIKit;
+#elif PLATFORM_ANDROID
+#else
+using System;
+#if !PLATFORM_UNITY
+using System.Collections.Concurrent;
+#endif
+using System.Diagnostics;
+using System.Reflection;
+#endif
+
+namespace Protobuild.FunctionalTests
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+#if PLATFORM_IOS
+            UIApplication.Main(args, null, "AppDelegate");
+#elif PLATFORM_ANDROID
+#else
+            if (Prototest.Library.Runner.Run(
+                Assembly.GetExecutingAssembly(),
+                args))
+            {
+                Environment.Exit(0);
+            }
+
+            Environment.Exit(1);
+#endif
+        }
+    }
+}
+
